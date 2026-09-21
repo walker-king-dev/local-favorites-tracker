b64349ef5d51ad4455a567c3abcc0123f6612e23
walkerwking <walker.king@tcu.edu>
2026-09-20 21:57:26 -0500

    Lab 12 Submission

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log('app.js connected');
+let myFavorite = {
+    name: 'Kentucky',
+    category: 'state',
+    rating: 5,
+    notes: 'Beautiful rolling green horse pastures',
+    dateAdded: 'Sep 2026'
+};
+
+let today = new Date().toLocaleDateString();
+console.log(today);
+
+console.log(myFavorite);
+console.log(typeof myFavorite.name);
+console.log(typeof myFavorite.rating)
