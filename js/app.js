e2987a1e6af2eed9369824cc7d42221c9a757746
walkerwking <walker.king@tcu.edu>
2026-09-21 10:28:31 -0500

    Lab 13 Submission

--- js/app.js
+++ js/app.js
@@ -12,3 +12,20 @@ console.log(today);
 console.log(myFavorite);
 console.log(typeof myFavorite.name);
 console.log(typeof myFavorite.rating)
+
+function greetFavorite(placeName, rating) {
+    console.log(placeName + ' has ' + rating + ' stars!');
+}
+greetFavorite('Kentucky', 5);
+
+const nameInput = document.getElementById('name');
+console.log(nameInput.value);
+
+const practiceForm = document.getElementById('add-favorite-form');
+
+function handleSubmit(event) {
+    event.preventDefault();   // stop the page reload
+    console.log('You typed: ' + nameInput.value);
+}
+
+practiceForm.addEventListener('submit', handleSubmit);
